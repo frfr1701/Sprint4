a37beb5df6ea99a2e545d754ef056e6a5c9fa56c
Ian Molander <ian.molander@gmail.com>
2017-11-16 18:15:12 +0100

    s

--- src/Client/StartPanel.java
+++ src/Client/StartPanel.java
@@ -9,23 +9,26 @@ class StartPanel extends JPanel {
     JButton newGame = new JButton("Nytt spel");
     JPanel panel = new JPanel();
     JLabel team = new JLabel(new ImageIcon("/Users/Ian/Desktop/test/export.png"));
-    
+    Color background = new Color(175, 175, 175);
     
     public StartPanel(ActionListener al) {
         newGame.addActionListener(al);
         exitGame.addActionListener(al);
         
+        
     }
     
     public void setPanel(){
         
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
         panel.setLayout(new BorderLayout());
         panel.add("North", newGame);
         panel.add("Center", team);
         panel.add("South", exitGame);
-        add(panel);
+        add("Center", panel);
+        setBackground(background);
          
+        
         
     }
     
