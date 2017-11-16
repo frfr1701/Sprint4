35171f15541607f4e84e45f19244ac885c68bb87
Jakob <jakob.lindahl@yh.nackademin.se>
2017-11-16 09:16:11 +0100

    c

--- src/Server/QuestionReader.java
+++ src/Server/QuestionReader.java
@@ -1,22 +1,23 @@
 package Server;
+
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 import java.util.stream.*;
 
-public class QuestionReader implements Serializable{
+public class QuestionReader implements Serializable {
 
     private final Path dir = Paths.get("src\\Server\\Resources");
     private DirectoryStream<Path> directoryStream;
-    protected List<String> subjects = new ArrayList<>();
-    protected List<String[]> questions = new ArrayList<>();
+    protected List<String> subjects;
+    protected List<String[]> questions;
 
     public QuestionReader() {
         try {
             directoryStream = Files.newDirectoryStream(dir, "*.{txt}");
             directoryStream.forEach((path) -> {
                 subjects.add(path.getFileName().toString().replace(".txt", ""));
-                fileReader(path);
+                questions.addAll(fileReader(path));
             });
         } catch (IOException ex) {
             System.out.println("Fel vid inläsning av textfiler från " + dir.toString());
@@ -26,15 +27,16 @@ public class QuestionReader implements Serializable{
 
     /**
      * //läser in 5 rader från en textfil och lägger dem i en array 
-     * //plats 0 är kategori
-     * //plats 1 är frågan
-     * //plats 2 är RÄTT svar
+     * //plats 1 är frågan 
+     * //plats 2 är RÄTT svar 
      * //plats 3 är fel svar 
-     * //plats 4 är fel svar
+     * //plats 4 är fel svar 
      * //plats 5 är fel svar
+     * //(Plats 0 i arrayen blir kategorin)
      * @param p path för filen som ska läsas in
      */
-    private void fileReader(Path p) {
+    private List<String[]> fileReader(Path p) {
+        List<String[]> tempQuestions = new ArrayList<>();
         String[] temp = new String[6];
         try (BufferedReader br = new BufferedReader(Files.newBufferedReader(p))) {
             while (((temp[1] = br.readLine()) != null)
@@ -43,26 +45,21 @@ public class QuestionReader implements Serializable{
                     && ((temp[4] = br.readLine()) != null)
                     && ((temp[5] = br.readLine()) != null)) {
                 temp[0] = p.getFileName().toString().replace(".txt", "");
-                questions.add(Arrays.copyOf(temp, temp.length));
+                tempQuestions.add(Arrays.copyOf(temp, temp.length));
             }
         } catch (IOException ex) {
             System.out.println("Fel vid inläsning av filen " + p.getFileName());
             System.out.println(ex.getCause());
         }
+        return tempQuestions;
     }
-    
+
     public List<String> getSubjects() {
         return subjects;
     }
-     /*
-     * @return returnerar ALLA frågor på specifierad kategori
-     */
+    
     public List<String[]> getQuestions(String subject) {
-        List<String[]> temp = questions.stream()
-                .filter((index) -> index[0]
-                .equalsIgnoreCase(subject))
-                .collect(Collectors.toList());
-        return temp;
+        return questions.stream().filter((index) -> index[0].equalsIgnoreCase(subject)).collect(Collectors.toList());
     }
 }
 
@@ -75,39 +72,38 @@ class QuestionsAndSubjects extends QuestionReader {
         this.NumberOfQuestions = NumberOfQuestions;
         this.NumberOfSubjects = NumberOfSubjects;
     }
-    /**
-     * @return Speciferat antal subjects (se konstruktor)
-     */
-    
+
     @Override
     public List<String> getSubjects() {
         return shuffleMaList(makeListFromSet(differentElements(subjects, NumberOfSubjects)));
     }
+
     @Override
     public List<String[]> getQuestions(String subject) {
         return shuffleMaList(makeListFromSet(differentElements(filteredBoiSubject(subject), NumberOfQuestions)));
     }
-    
-    private Set differentElements(List fromList, int antal){
+
+    private Set differentElements(List fromList, int antal) {
         Set<Object> UniqueElements = new HashSet<>();
         while (UniqueElements.size() < antal) {
             UniqueElements.add(giveRandomElement(fromList));
         }
         return UniqueElements;
     }
-    private Object giveRandomElement(List chosenOne){
-        return chosenOne.get((int)(Math.random() * chosenOne.size()));
-    }    
-    
-    private List<String[]> filteredBoiSubject(String filterByThisSubject){
+
+    private Object giveRandomElement(List chosenOne) {
+        return chosenOne.get((int) (Math.random() * chosenOne.size()));
+    }
+
+    private List<String[]> filteredBoiSubject(String filterByThisSubject) {
         return questions.stream().filter(indexOfList -> indexOfList[0].equalsIgnoreCase(filterByThisSubject)).collect(Collectors.toList());
     }
-    
-    private List makeListFromSet(Set set){
-        return (List)set.stream().collect(Collectors.toList());
+
+    private List makeListFromSet(Set set) {
+        return (List) set.stream().collect(Collectors.toList());
     }
-    
-    private List shuffleMaList(List shuffleMe){
+
+    private List shuffleMaList(List shuffleMe) {
         Collections.shuffle(shuffleMe);
         return shuffleMe;
     }
