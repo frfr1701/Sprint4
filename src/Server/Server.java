bbbcab31813b2b36f1506455cc60656546506245
frfr1701 <fredrik.frolund@yh.nackademin.se>
2017-11-16 10:12:05 +0100

    merge till jakob

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,62 +1,48 @@
 package Server;
 
 import Domain.Session;
-import Domain.test.Question;
-import Server.config.Config;
-//import com.fasterxml.jackson.databind.DeserializationFeature;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
+import java.io.*;
 import java.net.Socket;
 
-public class Server {
-    public static void main(String[] args) throws IOException {
-        
-        int portNumber = 44444;
+public class Server extends Thread {
 
-        try ( 
-            ServerSocket serverSocket = new ServerSocket(portNumber);
-            Socket clientSocket = serverSocket.accept();
+    Socket socketToClient;
+    Socket socketToClient2;
 
-        ) {
+    Server(Socket socketToClient, Socket socketToClient2) {
+        this.socketToClient = socketToClient;
+        this.socketToClient2 = socketToClient2;
+    }
 
-            Config config = new Config(); //TODO: Vart borde denna ligga?
-
-
-
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, false);
-//            Question[] questions = objectMapper.readValue(new FileInputStream("src/Domain/test/Questions.json"), Question[].class);
-
-
-            ObjectOutputStream oos= new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+    @Override
+    public void run() {
+        try (
+            ObjectOutputStream oos = new ObjectOutputStream(socketToClient.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(socketToClient.getInputStream());
+            ObjectOutputStream oos2 = new ObjectOutputStream(socketToClient2.getOutputStream());
+            ObjectInputStream ois2 = new ObjectInputStream(socketToClient2.getInputStream());
+             ){
             Session input, output;
-            
+
             // Initiate conversation with client
-            Protocol kkp = new Protocol();
-            output = kkp.processInput(kkp.getInitialSession());
+            Protocol protocol = new Protocol();
+            output = protocol.processInput(protocol.getInitialSession());
             oos.writeObject(output);
 
-            while ((input = (Session)ois.readObject()) != null) {
-                output = kkp.processInput(input);
+            while ((input = (Session) ois.readObject()) != null) {
+                output = protocol.processInput(input);
                 oos.writeObject(output);
-                
+
             }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
-                + portNumber + " or listening for a connection");
+                    + socketToClient.getPort() + " or listening for a connection");
             System.out.println(e.getMessage());
             e.printStackTrace();
-        }
-        catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             System.out.println("Class not found when trying to listen on port "
-                + portNumber + " or listening for a connection");
+                    + socketToClient.getPort() + " or listening for a connection");
             System.out.println(e.getMessage());
         }
     }
-} 
+}
