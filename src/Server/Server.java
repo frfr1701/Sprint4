01da6358efeb32b6a416d83becd21b462389942e
Jakob <jakob.lindahl@yh.nackademin.se>
2017-11-22 15:28:54 +0100

    s

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -5,38 +5,6 @@ import Domain.Session;
 import java.io.*;
 import java.net.Socket;
 
-<<<<<<< HEAD
-public class Server extends Thread{
-    Socket socketToClient;
-    Protocol protocol = new Protocol();
-    
-    public Server(Socket socketToClient) {
-        this.socketToClient=socketToClient;
-    }
-    
-    @Override
-    public void run(){
-        try ( 
-              ObjectOutputStream oos= new ObjectOutputStream(socketToClient.getOutputStream());
-              ObjectInputStream ois = new ObjectInputStream(socketToClient.getInputStream());
-        ) {
-
-            Session input, output;
-            
-            // Initiate conversation with client
-            Protocol kkp = new Protocol();
-            output = kkp.processInput(kkp.getInitialSession());
-            oos.writeObject(output);
-
-            while ((input = (Session)ois.readObject()) != null) {
-                output = kkp.processInput(input);
-                oos.writeObject(output);
-                
-            }
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                + socketToClient.getPort() + " or listening for a connection");
-=======
 class Server extends Thread {
 
     private final Socket socketToClientOne;
@@ -82,15 +50,10 @@ class Server extends Thread {
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
                     + socketToClientOne.getPort() + " or listening for a connection");
->>>>>>> fe7cc7f2f208a680673645482ddf5fbcff95f12b
             System.out.println(e.getMessage());
         } catch (ClassNotFoundException e) {
             System.out.println("Class not found when trying to listen on port "
-<<<<<<< HEAD
-                + socketToClient.getPort() + " or listening for a connection");
-=======
                     + socketToClientOne.getPort() + " or listening for a connection");
->>>>>>> fe7cc7f2f208a680673645482ddf5fbcff95f12b
             System.out.println(e.getMessage());
         }
     }
--- src/Server/ServerListener.java
+++ src/Server/ServerListener.java
@@ -4,35 +4,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-<<<<<<< HEAD
-public class ServerListener extends Thread{
-    
-    private ServerSocket serverSocket;;
-
-    public ServerListener() throws IOException {
-        this.serverSocket = new ServerSocket(12345);
-    }
-    
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    final Socket socketToClient = serverSocket.accept();
-                    Server clientHandler = new Server(socketToClient) {};
-                    clientHandler.start();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-    public static void main(String[] args) throws IOException {
-        ServerListener s = new ServerListener();
-        s.start();
-    }
-}
-
-=======
 class ServerListener {
 
     private Server server;
@@ -64,4 +35,3 @@ class ServerListener {
         }
     }
 }
->>>>>>> fe7cc7f2f208a680673645482ddf5fbcff95f12b
