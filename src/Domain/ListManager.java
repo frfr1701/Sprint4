558d9b75afa8984df16da48ae59fd8428f445885
Jakob <jakob.lindahl@yh.nackademin.se>
2017-11-22 09:43:46 +0100

    --

--- src/Domain/ListManager.java
+++ src/Domain/ListManager.java
@@ -5,17 +5,17 @@ import java.util.stream.Collectors;
 
 abstract class ListManger {
 
-    protected static List<String[]> getSessionQuestions(List<String[]> allQuestions, String whichSubject, int amountOfQuestions) {
+    static List<String[]> getSessionQuestions(List<String[]> allQuestions, String whichSubject, int amountOfQuestions) {
         return (List<String[]>) differentElements(filterBySubject(allQuestions, whichSubject), amountOfQuestions);
     }
 
-    protected static List<String> getSessionSubjects(List<String> allSubjects, int amountOfSubjects) {
+    static List<String> getSessionSubjects(List<String> allSubjects, int amountOfSubjects) {
         return (List<String>) differentElements(allSubjects, amountOfSubjects);
     }
     
     
     
-    protected static List<?> differentElements(List<?> fromList, int antal) {
+    static List<?> differentElements(List<?> fromList, int antal) {
         Set UniqueElements = new HashSet();
         while (UniqueElements.size() < antal) {
             UniqueElements.add(giveRandomElement(fromList));
@@ -23,15 +23,15 @@ abstract class ListManger {
         return (List<?>) UniqueElements.stream().collect(Collectors.toList());
     }
 
-    protected static Object giveRandomElement(List chosenOne) {
+    static Object giveRandomElement(List chosenOne) {
         return chosenOne.get((int) (Math.random() * chosenOne.size()));
     }
 
-    protected static List<String[]> filterBySubject(List<String[]> fromList, String filterByThisSubject) {
+    static List<String[]> filterBySubject(List<String[]> fromList, String filterByThisSubject) {
         return fromList.stream().filter(indexOfList -> indexOfList[0].equalsIgnoreCase(filterByThisSubject)).collect(Collectors.toList());
     }
 
-    protected static List shuffleList(List shuffleMe) {
+    static List shuffleList(List shuffleMe) {
         Collections.shuffle(shuffleMe);
         return shuffleMe;
     }
