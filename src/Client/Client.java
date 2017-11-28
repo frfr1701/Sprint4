7d9be279cf12f75c7a3624affdd3a6b967132c5a
Jakob <jakob.lindahl@yh.nackademin.se>
2017-11-28 21:23:59 +0100

    s

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -11,7 +11,7 @@ abstract class Client {
         Client start = new GameFrame();
         start.Client();
     }
-    
+
     private static final int PORTNUMBER = 44444;
     private static final String HOSTNAMNE = "127.0.0.1";
 
